43a68d8d7a33e6ed1d6a0e60fbec9d45c46a5a35
Adam Youlin He <ayh8@cornell.edu>
2023-11-12 09:23:27 -0500

    Update make.sh

--- pipeline/make.sh
+++ pipeline/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 n="$(($(nproc --all) / 2))"
-cores=${1:-n}
+cores=${1:-$n}
 
 echo snakemake --printshellcmds --reason --jobs $n --cores $cores --resources load=100
 snakemake --printshellcmds --reason --jobs $n --cores $cores --resources load=100
